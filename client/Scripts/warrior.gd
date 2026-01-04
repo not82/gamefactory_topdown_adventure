d0427655219ddce775787e1201ee06551fc96072
not82 <vendrouxf@gmail.com>
2026-01-04 15:49:43 +0100

    AnimationTree

--- client/Scripts/warrior.gd
+++ client/Scripts/warrior.gd
@@ -3,8 +3,17 @@ extends CharacterBody2D
 @export_category("Stats")
 @export var baseSpeed:float = 400	# En velocity par seconde
 
-@onready var animation : AnimationPlayer = $AnimationPlayer
+#@onready var animation : AnimationPlayer = $AnimationPlayer
 @onready var sprite : Sprite2D = $Sprite2D
+@onready var animationTree : AnimationTree = $AnimationTree
+@onready var animationPlayback : AnimationNodeStateMachinePlayback = $AnimationTree["parameters/playback"]
+
+enum State {
+	IDLE,
+	WALK
+}
+
+var state : State = State.IDLE
 
 #func _process(delta: float) -> void:
 func _process(delta: float) -> void:
@@ -20,17 +29,25 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("down"):
 		move.y += baseSpeed
 
-# Animationsd
-	if move == Vector2.ZERO:
-		animation.play("idle")	
-	else:
-		animation.play("walk")
+# Animations
+	if move == Vector2.ZERO && state == State.WALK:
+		state = State.IDLE
+		updateAnimation()
+	elif move != Vector2.ZERO && state == State.IDLE:
+		state = State.WALK
+		updateAnimation()
 
-	if move.x != 0:
-		if move.x < 0:
-			sprite.flip_h = true
-		else :
-			sprite.flip_h = false
+	if move.x < -0.01:
+		sprite.flip_h = true
+	elif move.x > 0.01:
+		sprite.flip_h = false
 		
 	set_velocity(move)	
 	move_and_slide()
+
+func updateAnimation() -> void:
+	match state:
+		State.IDLE:
+			animationPlayback.travel("idle")
+		State.WALK:
+			animationPlayback.travel("walk")

2cb79a401aca4c841b9cb2586dcfd7b8fc3c4f29
not82 <vendrouxf@gmail.com>
2026-01-04 11:02:24 +0100

    Just before AnimationPlayer

--- client/Scripts/warrior.gd
+++ client/Scripts/warrior.gd
@@ -1,36 +1,35 @@
 extends CharacterBody2D
 
-@export var baseSpeed = 10000
+@export_category("Stats")
+@export var baseSpeed:float = 400	# En velocity par seconde
 
 @onready var animation : AnimatedSprite2D = $AnimatedSprite2D
 
+#func _process(delta: float) -> void:
 func _process(delta: float) -> void:
-	velocity = Vector2()
+	var move = Vector2.ZERO
 
 #	Inputs
 	if Input.is_action_pressed("right"):
-		velocity.x = baseSpeed * delta
+		move.x += baseSpeed
 	if Input.is_action_pressed("left"):
-		velocity.x = - baseSpeed * delta
-	if Input.is_action_pressed("top"):
-		velocity.y = -baseSpeed * delta
+		move.x -= baseSpeed
+	if Input.is_action_pressed("up"):
+		move.y -= baseSpeed
 	if Input.is_action_pressed("down"):
-		velocity.y = baseSpeed * delta
+		move.y += baseSpeed
 
-# Animations
-	if velocity == Vector2.ZERO:
+# Animationsd
+	if move == Vector2.ZERO:
 		animation.play("idle")	
 	else:
 		animation.play("walk_right")
 
-	if velocity.x != 0:
-		if velocity.x < 0:
+	if move.x != 0:
+		if move.x < 0:
 			animation.flip_h = true
 		else :
 			animation.flip_h = false
 		
+	set_velocity(move)	
 	move_and_slide()
-	
-	
-#func _physics_process(delta: float) -> void:
-	#pass
